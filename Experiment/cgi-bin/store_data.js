29d94858851ab9f1478b3af6da0af2f5ca336c12
Brendan Robert <brobert@adobe.com>
2022-04-05 23:56:21 -0500

    adding logic to read post data

--- Experiment/cgi-bin/store_data.js
+++ Experiment/cgi-bin/store_data.js
@@ -1,10 +1,14 @@
 #!/bin/env /bin/node
 
+let postData = "";
+
 console.log("Access-Control-Allow-Origin:*");
 console.log("Access-Control-Allow-Headers:*");
 console.log("\n\n");
 console.log("Hello world!");
 console.log("This is node.js!");
+
+process.stdin.on('data', function(data) { postData += data; });
 
 process.argv.forEach((val, index) => {
     console.log(`${index}: ${val}`)
@@ -15,4 +19,7 @@ console.log(process.env)
 process.env.forEach((val) => {
     console.log(`${val}`)
 })
-console.log("\n\n");
+
+console.log(`Post data: ${postData}`);
+
+console.log("\r\n");
