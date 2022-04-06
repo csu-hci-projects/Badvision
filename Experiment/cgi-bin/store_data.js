b3ac84e66708ef3e1c0e6ad15323201eb9604097
Brendan Robert <brobert@adobe.com>
2022-04-06 01:18:43 -0500

    Fixed chunked transfer, working on form post read code now

--- Experiment/cgi-bin/store_data.js
+++ Experiment/cgi-bin/store_data.js
@@ -1,40 +1,42 @@
 #!/bin/env /bin/node
 
 let postData = "";
-let response = "";
+let responseBuffer = "";
 
 process.stdin.on('data', function(data) { postData += data; });
+process.stdin.on('end', () => main(postData));
 
 function print(data) {
-    response += data;
+    responseBuffer += data;
 }
 
 function println(data) {
-    response += data + "\n";
+    responseBuffer += data;
+    console.log(responseBuffer.length.toString(16) + "\r");
+    console.log(responseBuffer + "\r");
+    responseBuffer = "";
 }
 
-function sendResponse() {
+function main(postData) {
     console.log("Access-Control-Allow-Origin:*");
     console.log("Access-Control-Allow-Headers:*");
     console.log("Content-Type: text/plain");
     console.log("Transfer-Encoding: chunked");
-    console.log("\n");
-    console.log(response.length + "\r\n");
-    console.log(response + "\r\n");
+    console.log();
+
+    println("Hello world!\n");
+    println("This is node.js!");
+
+    process.argv.forEach((val, index) => {
+        println(`${index}: ${val}`)
+    })
+
+    for (const v in process.env) {
+        println(`${v} ${process.env[v]}`)
+    }
+
+    println(`Post data: ${postData}`);
+
+    // Needed to terminate the chunked stream
+    println("");
 }
-
-println("Hello world!");
-println("This is node.js!");
-
-process.argv.forEach((val, index) => {
-    println(`${index}: ${val}`)
-})
-
-for (const v in process.env) {
-    println(`${v} ${process.env[v]}`)
-}
-
-println(`Post data: ${postData}`);
-
-
-sendResponse();
